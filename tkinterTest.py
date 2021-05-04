f0fa38565a76ec13136c414b361e6dd95a7af838
LilyEmmanel <77690434+LilyEmmanel@users.noreply.github.com>
2021-05-04 14:19:37 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,29 +1,11 @@
-#tkinter docs
 from tkinter import *
+
+#tkinter docs
 top = Tk()
 
-playlist = []
+L1 = Label(top, text="Hello, world!")
 
-def addSong():
-    playlist.append(E1.get())
-
-def printList():
-    print(playlist)
-
-#this is a Label widget
-L1 = Label(top, text= "Playlist Generator")
 L1.grid(column= 0, row= 1)
-
-#This is a Text Entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column = 0, row = 2)
-
-#This is a Button widget
-B1 = Button(text= "Add to Playlist", bg = "light blue", command = addSong)
-B1.grid(column = 1, row =2)
-
-B2 = Button(text= "Export List", bg = "yellow", command = printList)
-B2.grid(column = 1, row =1)
 
 
 top.mainloop()
