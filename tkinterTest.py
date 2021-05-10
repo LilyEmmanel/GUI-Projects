c1094aac15c8519730d1e22e4965fc5719266589
LilyEmmanel <77690434+LilyEmmanel@users.noreply.github.com>
2021-05-10 14:27:08 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -10,21 +10,55 @@ def addSong():
 def printList():
     print(playlist)
 
-#this is a Label widget
-L1 = Label(top, text= "Playlist Generator")
-L1.grid(column= 0, row= 1)
+def exportPlaylist():
+    with open('playlist.txt', 'w') as f:
+        for song in playlist:
+            f.write("%s/n" % song)
 
-#This is a Text Entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column = 0, row = 2)
+def clearWindow():
+    for widget in top.winfo_children():
+        widget.destroy()
 
-#This is a Button widget
-B1 = Button(text= "Add to Playlist", bg = "light blue", command = addSong)
-B1.grid(column = 1, row =2)
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "Block 5 GUIs")
+    LMain.grid(column = 0, row = 1)
 
-B2 = Button(text= "Export List", bg = "yellow", command = printList)
-B2.grid(column = 1, row =1)
+    B1Main = Button(text = "Week 1", bg= "white", command = week1)
+    B1Main.grid(column = 0, row = 2)
 
+    B2Main = Button(text = "Week 2", bg = "white")
+    B2Main.grid(column = 0, row = 3)
 
-top.mainloop()
+    B3Main = Button(text = "Week 3", bg = "white")
+    B3Main.grid(column = 0, row = 4)
+
+def week1():
+    clearWindow()
+    #This is a Label widget
+    L1 = Label(top, text= "Playlist Generator")
+    L1.grid(column= 0, row= 1)
+
+    #This is a Text Entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column = 0, row = 2)
+
+    #This is a Button widget
+    B1 = Button(text= " + ", bg = "#B4F9FF", command = addSong)
+    B1.grid(column = 1, row =2)
+
+    B2 = Button(text= "Print List", bg = "white", command = printList)
+    B2.grid(column = 0, row =3)
+
+    B3 = Button(text= "Export List", bg = "8a7e42", command = exportPlayList)
+    B3.grid(column = 1, row =3)
+
+    """
+    Bclear = Button(text="Clear", bg = "white", command= clearWindow)
+    Bclear.grid(column = 20, row =40)
+    """
+
+if __name__ == "__main__":
+    mainMenu()
+    top.mainloop()
 
