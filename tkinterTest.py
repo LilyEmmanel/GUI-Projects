5603d9a46651154357cc7690ec9d82beb3c86658
LilyEmmanel <77690434+LilyEmmanel@users.noreply.github.com>
2021-05-11 14:25:02 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -4,9 +4,6 @@ top = Tk()
 
 playlist = []
 
-def addSong():
-    playlist.append(E1.get())
-
 def printList():
     print(playlist)
 
@@ -27,13 +24,18 @@ def mainMenu():
     B1Main = Button(text = "Week 1", bg= "white", command = week1)
     B1Main.grid(column = 0, row = 2)
 
-    B2Main = Button(text = "Week 2", bg = "white")
+    B2Main = Button(text = "Week 2", bg = "white", command = week2)
     B2Main.grid(column = 0, row = 3)
 
     B3Main = Button(text = "Week 3", bg = "white")
     B3Main.grid(column = 0, row = 4)
 
 def week1():
+
+    def addSong():
+        playlist.append(E1.get())
+        E1.delete(0, END)
+    
     clearWindow()
     #This is a Label widget
     L1 = Label(top, text= "Playlist Generator")
@@ -50,13 +52,33 @@ def week1():
     B2 = Button(text= "Print List", bg = "white", command = printList)
     B2.grid(column = 0, row =3)
 
-    B3 = Button(text= "Export List", bg = "8a7e42", command = exportPlayList)
+    B3 = Button(text= "Export List", bg = "#8a7e42", command = exportPlaylist)
     B3.grid(column = 1, row =3)
 
-    """
-    Bclear = Button(text="Clear", bg = "white", command= clearWindow)
+    Bclear = Button(text="Main Menu", bg = "white", command= mainMenu)
     Bclear.grid(column = 20, row =40)
-    """
+
+def week2():
+    clearWindow()
+
+    L1W2 = Label(top, text= "Dice Roller App")
+    L1W2.grid(column= 2, row= 1)
+    
+    L2W2 = Label(top, text= "# of Sides")
+    L2W2.grid(column= 1, row= 2)
+    
+    L3W2 = Label(top, text= "# of Rolls")
+    L3W2.grid(column= 3, row= 2)
+
+    E1W2 = Entry(top, bd = 5)
+    E1W2.grid(column= 1, row= 3)
+    
+    E2W2 = Entry(top, bd = 5)
+    E2W2.grid(column= 3, row= 3)
+
+    B1W2 = Button(text= "Roll 'em", bg = "yellow")
+    B1W2.grid(column= 2, row= 4) 
+   
 
 if __name__ == "__main__":
     mainMenu()
